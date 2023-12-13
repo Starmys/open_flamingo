9f915db1aa808e00128804f343cecf78d4424fab
Chengruidong Zhang <chengzhang@microsoft.com>
2023-12-13 08:27:47 +0000

    Update experiment settings

--- open_flamingo/scripts/run_eval_llava.sh
+++ open_flamingo/scripts/run_eval_llava.sh
@@ -55,43 +55,43 @@ export PYTHONPATH="$PYTHONPATH:open_flamingo"
 #     --ok_vqa_test_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_val2014_annotations.json" \
 #     --ok_vqa_test_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_val2014_questions.json"
 
-# python open_flamingo/eval/evaluate.py \
-#     --model llava \
-#     --model_base none \
-#     --model_path /home/chengzhang/models/llava/llava-v1.5-7b \
-#     --results_file "/home/chengzhang/Multimodal-Quantization/evaluation/LLaVA/results-1.5-w32a4-textvqa-lut-hybrid.json" \
-#     --precision fp16 \
-#     --quant_args w_bits=32,a_bits=4,act_quant_func="lut.hybrid",act_token_split=0 \
-#     --batch_size 4 \
-#     --shots 0 \
-#     --eval_ok_vqa \
-#     --ok_vqa_train_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/train2014" \
-#     --ok_vqa_train_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_train2014_annotations.json" \
-#     --ok_vqa_train_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_train2014_questions.json" \
-#     --ok_vqa_test_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/val2014" \
-#     --ok_vqa_test_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_val2014_annotations.json" \
-#     --ok_vqa_test_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_val2014_questions.json" \
-#     --eval_textvqa \
-#     --textvqa_image_dir_path "/home/chengzhang/datasets/TextVQA/images/train_images"
+python open_flamingo/eval/evaluate.py \
+    --model llava \
+    --model_base none \
+    --model_path /home/chengzhang/models/llava/llava-v1.5-7b \
+    --results_file "/home/chengzhang/Multimodal-Quantization/evaluation/LLaVA/results-1.5-w32a4-textvqa-lut-hybrid.json" \
+    --precision fp16 \
+    --quant_args w_bits=32,a_bits=4,act_quant_func="lut.hybrid",act_token_split=0 \
+    --batch_size 4 \
+    --shots 0 \
+    --eval_ok_vqa \
+    --ok_vqa_train_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/train2014" \
+    --ok_vqa_train_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_train2014_annotations.json" \
+    --ok_vqa_train_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_train2014_questions.json" \
+    --ok_vqa_test_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/val2014" \
+    --ok_vqa_test_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_val2014_annotations.json" \
+    --ok_vqa_test_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_val2014_questions.json" \
+    --eval_textvqa \
+    --textvqa_image_dir_path "/home/chengzhang/datasets/TextVQA/images/train_images"
 
-# python open_flamingo/eval/evaluate.py \
-#     --model llava \
-#     --model_base none \
-#     --model_path /home/chengzhang/models/llava/llava-v1.5-7b \
-#     --results_file "/home/chengzhang/Multimodal-Quantization/evaluation/LLaVA/results-1.5-w32a4-textvqa-lut-split.json" \
-#     --precision fp16 \
-#     --quant_args w_bits=32,a_bits=4,act_quant_func="lut",act_token_split=1 \
-#     --batch_size 4 \
-#     --shots 0 \
-#     --eval_ok_vqa \
-#     --ok_vqa_train_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/train2014" \
-#     --ok_vqa_train_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_train2014_annotations.json" \
-#     --ok_vqa_train_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_train2014_questions.json" \
-#     --ok_vqa_test_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/val2014" \
-#     --ok_vqa_test_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_val2014_annotations.json" \
-#     --ok_vqa_test_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_val2014_questions.json" \
-#     --eval_textvqa \
-#     --textvqa_image_dir_path "/home/chengzhang/datasets/TextVQA/images/train_images"
+python open_flamingo/eval/evaluate.py \
+    --model llava \
+    --model_base none \
+    --model_path /home/chengzhang/models/llava/llava-v1.5-7b \
+    --results_file "/home/chengzhang/Multimodal-Quantization/evaluation/LLaVA/results-1.5-w32a4-textvqa-lut-split.json" \
+    --precision fp16 \
+    --quant_args w_bits=32,a_bits=4,act_quant_func="lut",act_token_split=1 \
+    --batch_size 4 \
+    --shots 0 \
+    --eval_ok_vqa \
+    --ok_vqa_train_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/train2014" \
+    --ok_vqa_train_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_train2014_annotations.json" \
+    --ok_vqa_train_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_train2014_questions.json" \
+    --ok_vqa_test_image_dir_path "/home/chengzhang/datasets/OK-VQA/images/val2014" \
+    --ok_vqa_test_annotations_json_path "/home/chengzhang/datasets/OK-VQA/annotation/mscoco_val2014_annotations.json" \
+    --ok_vqa_test_questions_json_path "/home/chengzhang/datasets/OK-VQA/question/OpenEnded_mscoco_val2014_questions.json" \
+    --eval_textvqa \
+    --textvqa_image_dir_path "/home/chengzhang/datasets/TextVQA/images/train_images"
 
 python open_flamingo/eval/evaluate.py \
     --model llava \
